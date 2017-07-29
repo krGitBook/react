4db25824beafd5c709db7375f164167f50fd4503
dongfanai <dongfanai@krspace.cn>
2017-07-29 14:59:43 +0800

    feat:demo

--- src/Containers/Demo/Dfa/child.js
+++ src/Containers/Demo/Dfa/child.js
@@ -2,13 +2,16 @@ import React from 'react';
 
 export default class Child extends React.Component {
 	static contextTypes = {
-        name: React.PropTypes.object.isRequired
+        name: React.PropTypes.string.isRequired,
+        fn: React.PropTypes.func,
     }
 	render() {
 
 		return (
 				<div style={{marginBottom:"50px"}}>
 					子页面
+					<div>name:{this.context.name}</div>
+					{this.context.fn ()}
 				</div>
 	   );
 	}
--- src/Containers/Demo/Dfa/index.js
+++ src/Containers/Demo/Dfa/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { observer, inject } from 'mobx-react/custom';
 
+import Child from './child';
 @inject("WelComeModel")
 @observer
 export default class Dfa extends React.Component {
-	 static childContextTypes= {
-         name: React.PropTypes.string.isRequired
-    }
- getChildContext() {
+	 // static childContextTypes= {
+	 //        name: React.PropTypes.string.isRequired
+	 //   }
+ 	getChildContext() {
         return {
-            
-            
+            name:'张三',
+            age:20,
+            fn:function(){
+            	return 24
+            }
         };
     }
 	render() {
@@ -18,11 +22,14 @@ export default class Dfa extends React.Component {
 		return (
 				<div style={{marginBottom:"50px"}}>
 					父页面
+					<Child />
 				</div>
 	   );
 	}
 }
-// Dfa.wrappedComponent.childContextTypes = {
-//         refresh: React.PropTypes.func,
+Dfa.wrappedComponent.childContextTypes = {
+		name: React.PropTypes.string.isRequired,
+		age:React.PropTypes.number,
+        fn: React.PropTypes.func,
       
-// }
+}
