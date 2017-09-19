7ecf49832675af27c6b013b907863fd64e83f34e
刘毅豪 <liuyihao@krspace.cn>
2017-09-19 13:55:06 +0800

    pull

--- src/Components/MoveImage/index.js
+++ src/Components/MoveImage/index.js
@@ -4,9 +4,19 @@ import imageData from './images/demo.jpg'
 export default class MoveImage extends React.Component {
 
     componentDidMount(){
-        this.moveImage.addEventListener("move",this.transform,false);
+        this.moveImage.addEventListener("mousemove",this.transform,false);
     }
     transform = (event) =>{
+        var moveData = this.moveImage.getBoundingClientRect();
+        var halfX = moveData.width/2,halfY = moveData.height/2;
+        var contentX = moveData.left + halfX;
+        var contentY = moveData.top + halfY;
+        var mouseX = event.pageX;
+        var mouseY = event.pageY;
+        var ageX = 30*(mouseX - contentX )/halfX;
+        var ageY = 30*(contentY - mouseY)/halfY;
+        console.log(ageX,ageY,"PPPPP")
+        this.moveImage.style.transform= `perspective(600px) rotateY(${ageX}deg) rotateX(${ageY}deg)`;
         
     }
 
@@ -17,6 +27,7 @@ export default class MoveImage extends React.Component {
             
             <div className = "ui-move-image">
                 <img 
+                 className="move-transform"
                  ref = {(ref)=>{
                     this.moveImage = ref;
                  }}
