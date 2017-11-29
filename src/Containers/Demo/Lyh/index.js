2f69b4d54c727761bb4aaecb716b2a2966d3cfdf
wusuling <wusuling@krspace.cn>
2017-11-29 17:18:36 +0800

    feat:干掉file

--- src/Containers/Demo/Lyh/index.js
+++ src/Containers/Demo/Lyh/index.js
@@ -8,9 +8,7 @@ export default class Lyh extends React.Component {
 
 	constructor(props) {
 		super(props)
-		this.state={
-			 fileName:[]
-		}
+		
 	}
 
 	componentDidMount(){
@@ -18,45 +16,17 @@ export default class Lyh extends React.Component {
 	}
 
 	componentWillUnmount(){
-		this.state={
-			 fileName:[]
-		}
+		
 	}
 
-  onChange=(event)=>{
-		let e=event.target;
-		let {fileName}=this.state;
-		this.setState({
-			fileName:fileName.push(e.files[0])
-		})
-	}
+  
 
 	render() {
 
-		let {fileName}=this.state;
-
-		console.log('value',this.state.fileName);
-
+		
 		return (
-				<div style={{marginBottom:"50px"}} className='ui-icon'>
-
-				  <div className='icon-file'>+</div>
-
-					<input
-					 type="file"
-					 name="file"
-					 onChange={this.onChange.bind(this)}
-					 multiple/>
-
-
-					 <div>
-					 {
-						 fileName.map((item,index)=>{
-							  return  <div key={index} className='file-name'>{item.name}</div>
-						 })
-					 }
-					 </div>
-
+				<div>
+					123
 				</div>
 	   );
 	}
